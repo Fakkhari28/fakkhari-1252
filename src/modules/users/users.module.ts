0937f963176f32e12df65c77b7f4419c2bece4a6
Fakkhari Langyanai <Fakkhari2018@gmail.com>
2025-06-01 16:15:09 +0700

    feat: add passport-jwt dependency for JWT authentication

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -9,5 +9,6 @@ import { User } from 'src/entities';
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
